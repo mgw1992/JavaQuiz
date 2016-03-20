826a1b66ba07718257f4c103ef7cc791cb7ccde1
mgw1992 <meagan.wolf18@gmail.com>
2016-03-19 20:51:06 -0400

    Part Four-Week 3 Lab

--- app/src/main/java/com/example/dad/javaquiz/MainActivity.java
+++ app/src/main/java/com/example/dad/javaquiz/MainActivity.java
@@ -11,8 +11,11 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button button;
     private Button button2;
@@ -35,10 +38,41 @@ public class MainActivity extends AppCompatActivity {
         question_text_view.setText(question);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
         question_text_view = (TextView) findViewById(R.id.question_text_view);
